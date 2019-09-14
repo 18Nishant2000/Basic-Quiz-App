9c44f62de55f3d79647dc03f3f7b86e8ba1c4d6c
Nishant Bansal <bansal.nishant18052000@gmail.com>
2019-09-14 14:48:09 +0530

    first

---
+++ app/src/main/java/com/example/quiz/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.quiz;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    EditText email,password;
+    Button signup;
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        email=findViewById(R.id.editText);
+        password=findViewById(R.id.editText2);
+        signup=findViewById(R.id.button);
+
+        mAuth=FirebaseAuth.getInstance();
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String e=email.getText().toString();
+                String p=password.getText().toString();
+
+                mAuth.createUserWithEmailAndPassword(e,p).addOnCompleteListener(MainActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Registered Successfully...", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(MainActivity.this,firstActivity.class));
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Failed!!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+}
